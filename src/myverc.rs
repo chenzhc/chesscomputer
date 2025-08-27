78475d445392e7a44671cc63ac0598a578d3f2a4
chenzhichao <80139283@qq.com>
2025-08-27 23:02:32 +0800

    add

--- src/myverc.rs
+++ src/myverc.rs
@@ -1,6 +1,58 @@
 #![cfg_attr(debug_assertions, allow(dead_code, unused_imports, unused_variables, unused_mut))]
 
+use log::info;
 
+#[derive(Debug,Clone)]
+pub struct Car {
+    manufacturer: String,
+    model: String,
+}
+
+pub fn test_vec_car() {
+    let car_list  = vec![ Car { manufacturer: "Porsche".to_string(), model: "Panamera".to_string() }; 10];
+
+    info!("{:?}", car_list);
+
+    let mut car_list2: Vec<Car> = vec![];
+    let mut car_lot2: Vec<Car> = vec![];
+
+    for _ in 0..5_000_000u32 {
+        car_list2.push(Car { manufacturer: "Porshe".to_string(), model: "911".to_string() });
+    }
+
+    for _ in 0..100u8 {
+        car_lot2.push(Car { manufacturer: "Hyundai".to_string(), model: "Sonata".to_string() });
+    }
+
+    car_list2.append(&mut car_lot2);
+    car_list2.insert(0, 
+        Car { manufacturer: "Lamborghini".to_string(), model: "Aventador".to_string()}
+    );
+    car_list2.remove(0);
+
+    let keep = |e: &Car| {
+        if e.manufacturer == "Porshe" {
+            return true;
+        } else {
+            return false;
+        }
+    };
+    car_list2.retain(keep);
+
+    car_list2.reserve(5000);
+
+    // info!("{:?}", car_list2);
+    info!("Car list length: {:?}", car_list2.len());
+    info!("Car lot length: {:?}", car_lot2.len());
+    info!("{:?}", car_list2.capacity());
+    info!("{:?}", car_list2.get(0).unwrap());
+    
+    let mut input = String::new();
+
+    _ = std::io::stdin().read_line(&mut input).expect("Something bad happened");
+
+
+}
 
 
 
@@ -9,6 +61,17 @@ mod tests {
     use log::info;
 
     use super::*;
+
+    #[test]
+    fn it_vec_car_test() {
+        crate::init();
+        
+        let str_list = vec!["Trevor"; 10];
+        info!("{:?}", str_list);
+
+        test_vec_car();
+
+    }
 
     #[test]
     fn it_vec_string_test() {
