7f857e2f08f4be10cd71288b588083f52cbd3a4a
chenzhichao <80139283@qq.com>
2025-09-01 23:34:15 +0800

    add

--- src/mymutex.rs
+++ src/mymutex.rs
@@ -2,7 +2,7 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 
-use std::{ops::AddAssign, sync::Mutex, thread};
+use std::{ops::AddAssign, sync::Mutex, thread, time::Duration};
 
 use log::info;
 
@@ -23,22 +23,56 @@ pub fn test_mutex2() {
     let mut score = Mutex::new(0u16);
 
     let myfunc = || {
+        info!("Thread 1 is waiting for mutex lock...");
         let mut data = score.lock().unwrap();
         for i in 1..10 {
             data.add_assign(i);
+            info!("Thread 1 is adding {i}");
         }
     };
 
     let myfunc2 = || {
+        loop {
+            info!("Thread 2 is waiting for mutex lock...");
+            let guard = score.try_lock();
+
+            if guard.is_ok() {
+
+                let mut data = guard.unwrap();
+
+                for i in 1..10 {
+                    data.add_assign(i);
+                    info!("Thread 2 is adding {i}");
+                }
+                drop(data);
+
+            }
+
+            thread::sleep(Duration::from_millis(300));
+        }
+        
+        panic!("Error in thred 2");
+
+        
+    };
+
+    let myfunc3 = || {
+        info!("Thread 3 is waiting for mutex lock...");
         let mut data = score.lock().unwrap();
         for i in 1..10 {
             data.add_assign(i);
+            info!("Thread 3 is adding {i}");
         }
     };
 
     let _ = thread::scope(|scope| {
-        scope.spawn(myfunc);
-        scope.spawn(myfunc2);
+        let handle1 = scope.spawn(myfunc).join();
+        let handle2 = scope.spawn(myfunc2).join();
+        let handle3 = scope.spawn(myfunc3).join();
+
+        if handle2.is_err() {
+            info!("Thread 2 had a error, let's handle it here");
+        }
     });
 
     info!("{:?}", score.lock().unwrap());
